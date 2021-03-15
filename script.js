7cc6979e6a75ed71a3f04dace7d26e54980eaa20
Joemanni <78450152+Joemanni@users.noreply.github.com>
2021-03-15 16:19:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ function getDataFromItunes(){
               <img class="activator" src= "${song.artworkUrl100}">
             </div>
             <div class="card-content">
-              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right"></i></span>
+              <span class="card-title activator grey-text text-darken-4"><i class="material-icons right"></i></span>
               <p>${song.artistName}</p>
             </div>
             <div class="card-reveal">
-              <span class="card-title grey-text text-darken-4">${song.trackCensoredName}<i class="material-icons right">close</i></span>
+              <span class="card-title grey-text text-darken-4">${song.trackCensoredName}<i class="material-icons right"></i></span>
               //<p>Here is some more information about this product that is only revealed once clicked on.</p>
             </div>
           </div>
