dcc2211677b12463c8d3d29816d9cf08ba9cc428
Joemanni <78450152+Joemanni@users.noreply.github.com>
2021-03-15 10:26:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+let button = document.querySelector("#submit")
+let input = document.querySelector("#search")
+let output = document.querySelector("#output")
+
+button.addEventListener('click', (e) => {
+    getDataFromItunes()
+})
+
+function getDataFromItunes(){
+    let url = 'https://itunes.apple.com/search?term='+input.value
+    let cors = 'https://cors-anywhere.herokuapp.com/'
+    fetch(url)
+    .then( data => data.json())
+    .then( json => {
+        console.log(json)
+        let finalHTML = ''
+        json.results.forEach(song => {
+            finalHTML+= 
+            `
+            <div class="col s4 m4 l4"
+            <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src= "${song.artworkURL100}">
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+              <p>${song.artistName}</p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">${song.trackCensoredName}<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+          </div>
+            
+            `
+        })
+    })
+    .catch( error => console.log(error)())
+}
